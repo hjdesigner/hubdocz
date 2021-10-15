5b07d42b1978f67b2ab5a2dff11565a2eacc28c0
Henrique Melanda <hjdesigner23@gmail.com>
2021-10-15 18:02:13 -0300

    Created route api from create category and update category

---
+++ api/src/routes/category/createCategory.js
@@ -0,0 +1,35 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import commonMiddleware from '../../lib/commonMiddleware';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createCategory(event, context) {
+  const { name, status } = event.body;
+  const now = new Date();
+
+  const category = {
+    id: uuid(),
+    name,
+    status,
+    createAt: now.toISOString(),
+  };
+
+  try {
+    await dynamodb.put({
+      TableName: process.env.CATEGORY_TABLE_NAME,
+      Item: category,
+    }).promise();
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(category),
+  }
+}
+
+export const handler = commonMiddleware(createCategory)
---
+++ api/src/routes/category/updateCategory.js
@@ -0,0 +1,47 @@
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import commonMiddleware from '../../lib/commonMiddleware';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function updateCategory(event, context) {
+  const { name, status } = event.body;
+  const now = new Date();
+
+  const params = {
+    TableName: process.env.CATEGORY_TABLE_NAME,
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeNames: {
+      '#category_name': 'name',
+      '#category_status': 'status',
+    },
+    ExpressionAttributeValues: {
+      ':name': name,
+      ':status': status,
+      ':createAt': now.toISOString(),
+    },
+    UpdateExpression: 'SET #category_name = :name, #category_status = :status, createAt = :createAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  let update;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    console.log(result);
+    console.log(params);
+    update = result.Attributes;
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(update),
+  }
+}
+
+export const handler = commonMiddleware(updateCategory)
