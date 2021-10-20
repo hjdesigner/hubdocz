786b8f2e6ab98660061b4285e6e4d57b49fd668f
Henrique Melanda <hjdesigner23@gmail.com>
2021-10-19 21:29:47 -0300

    Create SubCategory yml and created endpoint from create sub category

---
+++ api/src/routes/subCategory/createSubCategory.js
@@ -0,0 +1,35 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import commonMiddleware from '../../lib/commonMiddleware';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createSubCategory(event, context) {
+  const { name, idCategory, status } = event.body;
+  const now = new Date();
+
+  const subCategory = {
+    id: uuid(),
+    name,
+    idCategory,
+    status,
+    createAt: now.toISOString(),
+  };
+
+  try {
+    await dynamodb.put({
+      TableName: process.env.SUB_CATEGORY_TABLE_NAME,
+      Item: subCategory,
+    }).promise();
+  } catch(error) {
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(subCategory),
+  }
+}
+
+export const handler = commonMiddleware(createSubCategory)
